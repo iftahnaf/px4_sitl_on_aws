51765548c4d536c799e69b61b2744c7c9d21bae6
Iftach Naftaly <iftahnaf@gmail.com>
2025-03-24 20:33:31 +0000

    fix: adding simulation launch file

---
+++ src/px4-external-module/launch/ci.launch.py
@@ -0,0 +1,65 @@
+from launch import LaunchDescription
+from launch.actions import (
+    ExecuteProcess
+)
+from ament_index_python.packages import get_package_share_directory
+import launch.logging
+import logging
+import os
+
+launch.logging.launch_config.level = logging.INFO
+
+parameters = os.path.join(
+        get_package_share_directory('px4-external-module'),
+        'config',
+        'params.yaml'
+    )
+
+
+def generate_launch_description():
+    px4_launch_command = (
+        "PX4_GZ_STANDALONE=1 PX4_SYS_AUTOSTART=4001 "
+        + "PX4_GZ_MODEL=gz_x500 ./build/px4_sitl_default/bin/px4"
+    )
+
+    px4_proc = ExecuteProcess(
+        cmd=[
+            "bash",
+            "-c",
+            f" cd /workspaces/px4_sitl_on_aws/PX4-Autopilot && {px4_launch_command}",
+        ],
+        output="screen",
+        emulate_tty=True,
+        name="px4",
+    )
+
+    gz_sim_command = [
+        "python3",
+        "/workspaces/px4_sitl_on_aws/PX4-Autopilot/Tools/simulation/gz/simulation-gazebo",
+        "--world",
+        "default",
+        "--model_store",
+        "/workspaces/px4_sitl_on_aws/PX4-Autopilot/Tools/simulation/gz/",
+    ]
+
+    gz_sim = ExecuteProcess(
+        cmd=gz_sim_command,
+        name="gz_sim",
+        output="screen",
+        emulate_tty=True,
+    )
+
+    node_dds_agent = ExecuteProcess(
+        cmd=["bash", "-c", "MicroXRCEAgent udp4 -p 8888"],
+        output="screen",
+        emulate_tty=True,
+    )
+
+    elements_to_launch = [
+        gz_sim,
+        node_dds_agent,
+        px4_proc,
+    ]
+
+    ld = LaunchDescription(elements_to_launch)
+    return ld
