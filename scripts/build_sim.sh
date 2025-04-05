d7e09c5598d81f5d411540f3fbeb549cfe570385
Iftach Naftaly <iftahnaf@gmail.com>
2025-04-05 07:41:29 +0000

    fix(build): correct repo name

--- scripts/build_sim.sh
+++ scripts/build_sim.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 set -e
 
-registry=$1
+registry="718459739973.dkr.ecr.eu-west-1.amazonaws.com"
+repo="px4_sitl_on_aws"
 timestamp=$(date +"%Y%m%d%H%M")
-image="$registry/simulation:$timestamp"
+
+image="$registry/$repo:$timestamp"
+
+echo "🔧 Building image: $image"
 
 docker build -t "$image" -f ./dockers/Dockerfile.simulation .
+
+echo "📦 Pushing image: $image"
 docker push "$image"
 
-docker tag "$image" "$registry/simulation:latest"
-docker push "$registry/simulation:latest"
+echo "🔁 Tagging and pushing 'latest'"
+docker tag "$image" "$registry/$repo:latest"
+docker push "$registry/$repo:latest"
