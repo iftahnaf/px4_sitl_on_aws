f4884beea733d2aec1f7018effd716e0e789b0d6
Iftach Naftaly <iftahnaf@gmail.com>
2025-04-18 10:42:48 +0000

    fix: launch args

--- scripts/simulation.sh
+++ scripts/simulation.sh
@@ -1,10 +1,28 @@
-#/bin/bash
+#!/bin/bash
+set -e
 
-# Source the required setup files
+# Source ROS and workspace setup
 source /opt/ros/jazzy/setup.bash
 source /workspaces/px4_sitl_on_aws/install/local_setup.bash
 
+# Install rosbags (once)
 pip install rosbags --break-system-packages
 
-# Launch the ROS2 simulation with the provided offsets
-ros2 launch px4_ci_aws ci.launch.py radius:=$1 altitude:=$2 omega:=$3 timeout_s:=$4
+# Read input args from workflow
+RADIUS="$1"
+ALTITUDE="$2"
+OMEGA="$3"
+TIMEOUT_S="$4"
+
+# Echo input args
+echo "Radius: $RADIUS"
+echo "Altitude: $ALTITUDE"
+echo "Omega: $OMEGA"
+echo "Timeout: $TIMEOUT_S"
+
+# Launch the ROS 2 simulation
+ros2 launch px4_ci_aws ci.launch.py \
+  radius:=$RADIUS \
+  altitude:=$ALTITUDE \
+  omega:=$OMEGA \
+  timeout_s:=$TIMEOUT_S
